94d7d011549331e183c318deb76cc0dbaa4f5d9e
Heba <121447741+hebafaisal@users.noreply.github.com>
2024-11-10 16:48:13 +0300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,44 +1,78 @@
-import { Controller, Get, Post, Put, Delete, Query } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entities';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
 
+  // Create a new user
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    try {
+      return await this.userService.create(createUserDto);
+    } catch (error) {
+      throw new HttpException('Error creating user', 400);
+    }
+  }
+
+  // Find all users
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async findAll(): Promise<User[]> {
+    try {
+      return await this.userService.findAll();
+    } catch (error) {
+      throw new HttpException('Error retrieving users', 400);
+    }
   }
 
-  @Get('/get-data')
-  getData(): any {
-    return {
-      uniId: '1',
-      name: 'Hala',
-      major: 'CSE',
-    };
+  // Find a user by ID
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<User> {
+    try {
+      const user = await this.userService.findOne(id);
+      if (!user) {
+        throw new HttpException('User not found', 404);
+      }
+      return user;
+    } catch (error) {
+      throw new HttpException('Error retrieving user', 400);
+    }
   }
 
-  @Post('/post-data')
-  postData(
-    @Query('uniId') uniId: number,
-    @Query('name') name: string,
-    @Query('major') major: string,
-  ) {
-    return {
-      uniId: uniId,
-      name: name,
-      major: major,
-    };
+  // Update a user by ID
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    try {
+      const updatedUser = await this.userService.update(id, updateUserDto);
+      return updatedUser;
+    } catch (error) {
+      throw new HttpException('Error updating user', 400);
+    }
   }
 
-  @Put('/put-data')
-  putData(): string {
-    return 'put';
-  }
-
-  @Delete('/delete-data')
-  deleteData(): string {
-    return 'delete';
+  // Remove a user by ID
+  @Delete(':id')
+  async remove(@Param('id') id: number): Promise<void> {
+    try {
+      await this.userService.remove(id);
+    } catch (error) {
+      throw new HttpException('Error deleting user', 400);
+    }
   }
 }
