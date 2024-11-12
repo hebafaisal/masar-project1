93fcca961c54ca29fa672f69440b50c329b4e2dc
Heba <121447741+hebafaisal@users.noreply.github.com>
2024-11-12 20:46:36 +0300

    Update user

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { UserService } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const entitiesPath = __dirname + '/**/*.entity{.ts,.js}';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306, 
+      username: 'root',
+      password: '',
+      database: 'masart_db',
+      entities: [entitiesPath],
+      synchronize: true,
+      logging: false,
+
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
-export class UserModule {}
+export class AppModule {}
