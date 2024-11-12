8e64201806843f91ab8d68697e7182886550cd35
Heba <121447741+hebafaisal@users.noreply.github.com>
2024-11-12 20:47:10 +0300

    Update user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/User.entity';
+import { Repository } from 'typeorm';
+import { CreateUserParams, UpdateUserParams } from './utils/types';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    constructor(@InjectRepository(User) private userRepository: Repository<User>) { }
+  createUser(userDetails: CreateUserParams) {
+    const newUser = this.userRepository.create({ ...userDetails });
+    return this.userRepository.save(newUser);
   }
 
-  findAll() {
-    return `This action returns all user`;
+  findUser() {
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  update(id: number, updateUserDetails: UpdateUserParams) {  
+    return this.userRepository.update({ id }, {...updateUserDetails});
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+  delete(id: number) {
+    return this.userRepository.delete({id});
+      }
 }
